1589c05ec5756c683f176197b881d39b14bc2de1
Chris Roman <Chris.FigueroaPerez@gmail.com>
2023-02-13 11:03:29 -0500

    Minor changes to function name (main_system -> day_trip_generator). Refactoring of the display_dictionary function. cleaned up challenge folder

--- day_trip_coding_challenge.py
+++ day_trip_coding_challenge.py
@@ -1,13 +1,2 @@
 import random as r
-
-while "yes" != input('\nHow about this for your day trip?\n'+
-    str({key2:r.choice(value2) for (key2,value2) in 
-        {
-            "Destination": ["Atlanta", "Marietta", "Kennesaw", "Acworth", "Woodstock", "Dunwoody"], 
-            "Restaurant": ["Wendy.s", "Sub-way", "Fogo de Chao", "Stoney River", "Rotating Sushi Bar"], 
-            "Entertainment": ["Concert", "Piedmont Park", "Centennial Park", "World of Coke", "Movie", "Georgia Aquarium", "Belt Line", "Atlanta Zoo"], 
-            "Transportation": ["Car", "Bike", "Uber", "Marta", "Friend.s Car"]
-            }.items()
-        }.items()
-    )[13:-3].replace(" (","").replace("),","\n").replace(",",":").replace("\"","").replace("'","").replace(".","'")
-+"\nWhat do you think [Yes, No]: ") : continue
+while "yes" != input('\nHow about this for your day trip?\n'+ str({key:r.choice(value) for (key,value) in {"Destination": ["Atlanta", "Marietta", "Kennesaw", "Acworth", "Woodstock", "Dunwoody"], "Restaurant": ["Wendy.s", "Sub-way", "Fogo de Chao", "Stoney River", "Rotating Sushi Bar"], "Entertainment": ["Concert", "Piedmont Park", "Centennial Park", "World of Coke", "Movie", "Georgia Aquarium", "Belt Line", "Atlanta Zoo"], "Transportation": ["Car", "Bike", "Uber", "Marta", "Friend.s Car"]}.items()}.items())[13:-3].replace(" (","").replace("),","\n").replace(",",":").replace("\"","").replace("'","").replace(".","'")+'\nWhat do you think [Yes, No]: ') : continue
--- day_trip_generator.py
+++ day_trip_generator.py
@@ -1,5 +1,4 @@
 import random as r
-
 def main():
     #Create Ditionary
     master_dictionary = {
@@ -10,27 +9,27 @@ def main():
     }
 
     #displays dictionary with key and value w/ prompt
-    def display_dictionary(prompt, dictionary):
-        print(f"{prompt}")
-        for key, item in dictionary.items():
-            print(f'{key} - {item}')
+    def display_dictionary(dictionary: dict, prompt = None):
+        if prompt != None:
+            print(f'{prompt}')
+        {print(f'{key} - {item}') for key, item in dictionary.items()}
 
     #Validate user input
-    def verify_user_response(test, valid_input):
+    def verify_user_response(test, valid_input: dict):
         try: 
             return ([valid_input[key] for key in valid_input.keys() if key == test.lower()][0])
         except: 
-            return verify_user_response(input(f"\nInvalid Entry \nExceptable answers are {list(valid_input.keys())}: "), valid_input)
+            return verify_user_response(input(f'\nInvalid Entry \nExceptable answers are {list(valid_input.keys())}: '), valid_input)
     
-    #Main system that runs program
-    def run_system():
+    #Main system that runs program (Randomizes Selections)
+    def day_trip_generator():
         selections = {key:r.choice(value) for (key,value) in master_dictionary.items()}
 
-        display_dictionary("\nDay Trip: ", selections)
-        if verify_user_response(input("Do you want this list: "),{"yes": True, "no": False}):
-           display_dictionary("\nYou have selected The following as your plans:", selections)
+        display_dictionary(selections, '\nDay Trip: ')
+        if verify_user_response(input('Do you want this list: '),{'yes': True, 'no': False}):
+           display_dictionary(selections, '\nYou have selected The following as your plans:')
            return True
-    return run_system()
+    return day_trip_generator()
 
 if __name__ == '__main__':
     while True:
